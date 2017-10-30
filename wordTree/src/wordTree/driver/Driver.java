2924ecf78cd82f44b03343757fff75532496ceb0
Michael Plohetski <mplohet1@cs.binghamton.edu>
2017-10-30 00:33:30 -0400

    Initial Framework

---
+++ wordTree/src/wordTree/driver/Driver.java
@@ -0,0 +1,7 @@
+package wordTree.driver;
+
+public class Driver {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ wordTree/src/wordTree/store/Results.java
@@ -0,0 +1,8 @@
+package wordTree.store;
+
+import wordTree.util.FileDisplayInterface;
+import wordTree.util.StdoutDisplayInterface;
+
+public class Results implements FileDisplayInterface, StdoutDisplayInterface{
+	
+}
