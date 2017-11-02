dd3ef48322de7a01ee9900244a0b040b101f6952
Michael Plohetski <mplohet1@cs.binghamton.edu>
2017-11-01 22:45:38 -0400

    Baseline for Thread classes

--- wordTree/src/wordTree/threadMgmt/DeleteThread.java
+++ wordTree/src/wordTree/threadMgmt/DeleteThread.java
@@ -1,5 +1,23 @@
 package wordTree.threadMgmt;
 
-public class DeleteThread {
+import wordTree.store.Results;
+import wordTree.util.FileProcessor;
+import wordTree.trees.Node;
+
+public class DeleteThread extends Thread {
+	private FileProcessor in;
+	private Results out;
+	private Node head;
+	private String word;
 	
+	public PopulateThread(FileProcessor f, Results r, Node n, String s) {
+		in = f;
+		out = r;
+		head = n;
+		word = s;
+	}
+	
+	public void run() {
+		head.delete(word);
+	}
 }
--- wordTree/src/wordTree/threadMgmt/PopulateThread.java
+++ wordTree/src/wordTree/threadMgmt/PopulateThread.java
@@ -1,5 +1,25 @@
 package wordTree.threadMgmt;
 
-public class PopulateThread {
+import wordTree.store.Results;
+import wordTree.util.FileProcessor;
+import wordTree.trees.Node;
+
+public class PopulateThread extends Thread {
+	private FileProcessor in;
+	private Results out;
+	private Node head;
 	
+	public PopulateThread(FileProcessor f, Results r, Node n) {
+		in = f;
+		out = r;
+		head = n;
+	}
+	
+	public void run() {
+		String word = in.readWord();
+		while (word != null) {
+			head.populate(word);
+			word = in.readWord();
+		}
+	}
 }
